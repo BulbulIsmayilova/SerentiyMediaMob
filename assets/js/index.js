c0d1cb72f374603e260be0b79a4de2eb30cde9eb
Bulbul <ismayilovabulbul29@gmail.com>
2024-11-13 17:38:51 +0400

    final

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,7 @@ let width = window.innerWidth;
 const flag1 = document.getElementById("flag1");
 const flag2 = document.getElementById("flag2");
 const flag3 = document.getElementById("flag3");
+const flagContainFlags = document.querySelectorAll(".flagContain-flags > img")
 
 document.addEventListener("DOMContentLoaded", function () {
   function updateFlagEvents() {
@@ -14,10 +15,22 @@ document.addEventListener("DOMContentLoaded", function () {
       flag1.addEventListener("click", openFlag);
       flag2.addEventListener("click", openFlag);
       flag3.addEventListener("click", openFlag);
+      flag1.removeEventListener("mouseover", hoverOpenFlag)
+      flag2.removeEventListener("mouseover", hoverOpenFlag)
+      flag3.removeEventListener("mouseover", hoverOpenFlag)
+      flagContain.removeEventListener("mouseleave", function(){
+        flagContain.style.display = "none"
+      })
     } else {
       flag1.removeEventListener("click", openFlag);
       flag2.removeEventListener("click", openFlag);
       flag3.removeEventListener("click", openFlag);
+      flag1.addEventListener("mouseover", () => hoverOpenFlag(0))
+      flag2.addEventListener("mouseover", () => hoverOpenFlag(1))
+      flag3.addEventListener("mouseover", () => hoverOpenFlag(2))
+      flagContain.addEventListener("mouseleave", function(){
+        flagContain.style.display = "none"
+      })
     }
   }
   updateFlagEvents();
@@ -32,3 +45,17 @@ const openFlag = () => {
   }
   flag = !flag;
 };
+
+let flagArr = ['flag1.svg', 'flag2.svg', 'flag3.svg']
+let flagActiveArr = ['active1.png','active1.png','active1.png']
+
+const hoverOpenFlag = (index) => {
+  for (let i = 0; i < flagContainFlags.length; i++) {
+    if(i == index){
+      flagContainFlags[i].src = `assets/img/${flagActiveArr[i]}`
+    }else{
+      flagContainFlags[i].src = `assets/img/${flagArr[i]}`
+    }
+  }
+  flagContain.style.display = "block"
+}
